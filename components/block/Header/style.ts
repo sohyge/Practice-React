a2b592d636dd477f0e931f1b97e1c4e7263642c4
HyukGeun Song <wck_os12@wconcept.co.kr>
2022-10-11 18:03:37 +0900

    dark mode removed

--- components/block/Header/style.ts
+++ components/block/Header/style.ts
@@ -5,8 +5,8 @@ export const Header = styled.header`
   top: 0;
   left: 0;
   right: 0;
-  background-color: #fff;
-  color: #000;
+  background-color: #000;
+  color: #fff;
   text-align: center;
   margin: 0 auto;
   line-height: 50px;
--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -4,7 +4,7 @@ import MainLayout from '../../components/layout/MainLayout';
 function test({}: NextPage) {
   return (
     <>
-      <MainLayout title="하이루 반가워요">
+      <MainLayout title="Test Page">
         무야호
         <br />
         무야호
