b08d2f969918e1927c1c91bfbddb4e47938e3bb3
HyukGeun Song <wck_os12@wconcept.co.kr>
2022-10-20 11:57:33 +0900

    chcheKey added

---
+++ libs/utils/cacheKey.ts
@@ -0,0 +1,5 @@
+export const CACHE_KEY = {
+  TRAFFIC: {
+    TRAFFIC_INFO: 'TRAFFIC_INFO',
+  },
+};
--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { useCallback } from 'react';
 import { getTrafficInfo } from '../../libs/apis/openAPI';
+import { CACHE_KEY } from '../../libs/utils/cacheKey';
 
 function Example() {
-  const { isLoading, error, data } = useQuery(['repoData'], () => getTrafficInfo());
+  const { isLoading, error, data } = useQuery([CACHE_KEY.TRAFFIC.TRAFFIC_INFO], () => getTrafficInfo());
 
   const getInfo = useCallback(() => {
     return <div>{data?.list[0].cbssu_bus}</div>;
