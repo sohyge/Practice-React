5b47e3b3c9d3d72672e8dfc642ae0c4a1ad04c0c
HyukGeun Song <wck_os12@wconcept.co.kr>
2022-11-08 15:06:59 +0900

    이거 뭔가 잘못됐어 재미가없어

--- libs/apis/openAPI.ts
+++ libs/apis/openAPI.ts
@@ -1,13 +1,20 @@
 import axios from 'axios';
 import { locationInfo } from '../types/location';
-import { trafficInfo } from '../types/traffic';
+import { trafficInfo, trafficRealInfo } from '../types/traffic';
 
 const KEY = '9179870253';
 const INFO_URL = `http://data.ex.co.kr/openapi/safeDriving/forecast?key=${KEY}&type=json`;
+const TRAFFIC_REAL_API = `http://data.ex.co.kr/openapi/trafficapi/trafficAll?key=${KEY}&type=json`;
 const LOCATION_INFO_URL = `http://data.ex.co.kr/openapi/locationinfo/locationinfoUnit?key=${KEY}&type=json`;
 
 export interface TrafficInfoResponse {
   list: trafficInfo;
+  count: number;
+  message: string;
+  code: string;
+}
+export interface TrafficRealInfoResponse {
+  trafficAll: trafficRealInfo;
   count: number;
   message: string;
   code: string;
@@ -18,8 +25,15 @@ export interface LocationInfoResponse {
   message: string;
   code: string;
 }
+
 export const getTrafficInfo = async () => {
   const { data } = await axios.get<TrafficInfoResponse>(INFO_URL);
+  console.log('suc', data);
+  return data;
+};
+
+export const getTrafficRealInfo = async () => {
+  const { data } = await axios.get<TrafficRealInfoResponse>(TRAFFIC_REAL_API);
   console.log('suc', data);
   return data;
 };
--- libs/types/traffic.d.ts
+++ libs/types/traffic.d.ts
@@ -37,3 +37,18 @@ export type trafficInfo = {
   ckrsu_bus: string; //남양주 -> 양양 버스 소요시간
   cyysu_bus: string; //양양 -> 남양주 버스 소요시간
 };
+
+export type trafficRealInfo = {
+  exDivName: string; //도공/민자 구분명
+  tmName: string; //자료구분명
+  tcsName: string; //TCS/hi-pass구분명
+  trafficAmout: string; //교통량
+  sumTm: string; //집계시간
+  carType: string; //차종구분코드(1:1종,2:2종,3:3종,4:4종,5:5종,6:6종,7:7종,8:8종)
+  exDivCode: string; //도공/민자 구분코드(도공:00/이외 민자)
+  tcsType: string; //TCS/hi-pass 구분(1:TCS,2:hi-pass)
+  tmType: string; //자료구분(1:1시간, 2:15분, 3:5분)
+  code: string; //결과
+  message: string; //결과 메시지
+  count: string; //전체 결과 수
+};
