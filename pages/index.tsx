57872d7636f35ef8f4807e46b78d854f9774ded6
HyukGeun Song <wck_os12@wconcept.co.kr>
2022-11-09 14:24:10 +0900

    cho gi hwa

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,7 @@ import { useEffect } from 'react';
 import router from 'next/router';
 
 function index({}: NextPage) {
-  useEffect(() => {
-    router.push('/traffic');
-  }, []);
+  useEffect(() => {}, []);
   return <></>;
 }
 
