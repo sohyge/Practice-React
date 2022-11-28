ed4e0b9026d9cbab0ca8080d63f9949d2bebd30f
HyukGeun Song <wck_os12@wconcept.co.kr>
2022-11-28 14:07:36 +0900

    디자인 업데이트

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
 const Square = styled.div`
-  width: 15vw;
-  height: 20vh;
-  border: 1px #000 solid;
+  width: 15vh;
+  height: 15vh;
+  border: 1px #fff solid;
+  border-radius: 10px;
+  background-color: orange;
+  color: #fff;
+  font-size: 7vh;
   text-align: center;
+  line-height: 15vh;
 `;
 
 export default Square;
